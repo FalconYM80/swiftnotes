d62964f7255818ac19ca440d8b4e9f1d8d8e2fbe
FalconYM80 <shanks1pees@gmail.com>
2025-06-27 23:50:54 +0530

    Working frontend

--- notes-frontend/src/components/Home.js
+++ notes-frontend/src/components/Home.js
@@ -38,7 +38,7 @@ const Home = () => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
           </svg>
-          <h2 className="text-xl font-semibold text-blue-600">Notes App</h2>
+          <h2 className="text-xl font-semibold text-blue-600">SwiftNote</h2>
         </div>
         <div className="flex items-center">
           <span className="mr-4 text-gray-700">Hello, {userName}</span>
